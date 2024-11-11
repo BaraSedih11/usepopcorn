d0bf9e12a1e7fc3fede9168330fd55faf3ec363c
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2024-11-11 19:19:47 +0200

    Done ✅✅✅✅✅

---
+++ src/useMovies.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+const KEY = "9e4f068f";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      //   callback?.();
+
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+
+          const res = await fetch(
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok) throw new Error(`Movie Not Found`);
+
+          const data = await res.json();
+
+          if (data.Response === "False") throw new Error(data.Error);
+
+          setMovies(data.Search);
+        } catch (error) {
+          if (error.name !== "AbortError")
+            setError("Network response was not ok.");
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (!query.length) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
